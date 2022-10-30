7d9985879ea5baccedc4d27bd64739cc4da34330
twDevNoob <smelowsergey@gmail.com>
2022-10-30 23:53:04 +0600

    Start with the script

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(".inputs")
+
+/* Controls */
+
+const billInput = document.querySelector(".input--bill")
+const customTipInput = document.querySelector(".input--custom-tip")
+const numberOfPeopleInput = document.querySelector(".input--number-of-people")
+
+const selectTipButtons = document.querySelector(".button--select-tip")
+
+const resetButton = document.querySelector(".button--reset")
+
+/* Output fields */
+
+const outputTipAmount = document.querySelector("#result-tip-amount")
+const outputTotal = document.querySelector("#result-total")
+
+/* User input validation */
